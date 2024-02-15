27021217f1d06d3b3c5a368d2026d7dcb4d81433
Mathias Balling <math260r@gmail.com>
2024-02-15 11:51:52 +0100

    Working with interrupts on Systick and GPIO

--- assignment_1/main.c
+++ assignment_1/main.c
@@ -1,15 +1,17 @@
-#include "tm4c123gh6pm.h"
+/**************************Includes**********************************/
 #include "systick.h"
+#include "tm4c123gh6pm.h"
 #include <stdbool.h>
 #include <stdint.h>
 
+/**************************Defines**********************************/
 // Missing definitions in tm4c123gh6pm.h file
 #define NVIC_INT_CTRL_UNPEND_SYST 0x02000000 // Unpend a systick int
 
-#define doubleClickTimeout 100  // Timeout for double click
-#define longPressTimeout   2000 // Timeout for long press
-#define debounceTimeout    20   // Debounce timeout
-#define autoModeInterval   200  // Auto mode interval
+#define DOUBLE_CLICK_TIMEOUT 100 // Timeout for double click
+#define LONG_PRESS_TIMEOUT 2000  // Timeout for long press
+#define DEBOUNCE_TIMEOUT 10      // Debounce timeout
+#define AUTO_MODE_INTERVAL 200   // Auto mode interval
 
 // LED colors
 enum LEDColor {
@@ -38,8 +40,9 @@ enum Direction {
 // Define the order of led colors
 enum LEDColor colors[] = {OFF, GREEN, BLUE, CYAN, RED, YELLOW, MAGENTA, WHITE};
 
+/**************************Variables**********************************/
 // Counter values. This is used as an intex into `colors`
-int counter = 0;
+volatile int counter = 0;
 
 // Initial contitions
 enum Mode mode = MANUAL;
@@ -48,25 +51,28 @@ enum Direction direction = UP;
 // The ticks counter is used for timing and updated by the `systick.h` module
 extern volatile uint32_t ticks;
 
+// The elapsed_ticks counter is used for timing
+volatile uint32_t last_press_tick = 0;
+volatile uint32_t last_release_tick = 0;
+
+/**************************Functions**********************************/
 // Check if the button is currently pressed. Returns 0 if not.
-int button_pressed() {
-    return ~(GPIO_PORTF_DATA_R) & 0b00010000;
-}
+int button_sw1_pressed() { return ~(GPIO_PORTF_DATA_R) & 0b00010000; }
 
 // Get the modulo of a number (even negative ones)
 int mod(int x, int N) { return (x % N + N) % N; }
 
 // Increment or decrement the counter
 void ment() {
-  if (direction) counter++;
-  else counter--;
+  if (direction)
+    counter++;
+  else
+    counter--;
   counter = mod(counter, 8);
 }
 
 // Set the color of the LED
-void setLEDColor(enum LEDColor color) {
-  GPIO_PORTF_DATA_R = color << 1;
-} 
+void setLEDColor(enum LEDColor color) { GPIO_PORTF_DATA_R = color << 1; }
 
 void setup() {
   int dummy;
@@ -88,79 +94,89 @@ void setup() {
 
   init_systick();
 
-  // Enable button interupt
-  NVIC_EN0_R |= (1<<30);
+  GPIO_PORTF_IS_R &= ~(1 << 4); // PF4 is edge-sensitive
+  GPIO_PORTF_IBE_R |= (1 << 4); // PF4 is both edges
+  GPIO_PORTF_IM_R = (1 << 4);   // Interrupt on PF4
+  GPIO_PORTF_ICR_R = (1 << 4);  // clear flag4
 
-  // Set interupt mask
-  GPIO_PORTF_IM_R = 0x10;
+  // Enable GPIOF interupt
+  NVIC_EN0_R |= (1 << 30);
 
-  GPIO_PORTF_ICR_R |= 0x10; // Clear interrupt bit
+  // Set priority to 5 (lower priority than systick)
+  NVIC_PRI7_R = (NVIC_PRI7_R & 0xFF00FFFF) | 0x00A00000;
+}
+
+void process_press() {
+  uint32_t press_duration = last_release_tick - last_press_tick;
+  if (press_duration < DEBOUNCE_TIMEOUT) {
+    // Filter out debouncing
+  } else if (press_duration >= LONG_PRESS_TIMEOUT) {
+    // Long press
+    mode = AUTO;
+  } else {
+    // Short press
+
+    // Temporary disable the interrupt on PF4
+    GPIO_PORTF_IM_R = (0 << 4); // Disable interrupt on PF4
+
+    // Turn off the auto mode
+    mode = MANUAL;
+
+    // Reset the counter
+    ticks = 0;
+    bool double_click = false;
+
+    // Wait for a potential double click
+    while (ticks < DOUBLE_CLICK_TIMEOUT) {
+      if (button_sw1_pressed() && ticks > DEBOUNCE_TIMEOUT) {
+        double_click = true;
+        while (button_sw1_pressed()) {
+          // Wait for the button to be released
+        }
+        break;
+      }
+    }
+    // Handle the press type
+    if (double_click) {
+      direction = direction == UP ? DOWN : UP;
+    } else {
+      ment();
+    }
+
+    // Re-enable the interrupt on PF4
+    GPIO_PORTF_ICR_R |= 0x10;   // Clear interrupt bit
+    GPIO_PORTF_IM_R = (1 << 4); // Interrupt on PF4
+  }
 }
 
 void IntButtonHandler(void) {
-
-
-  while (button_pressed());
-  uint32_t pressed_ticks = ticks;
-
-  counter++;
-
-  // Ignore switch bounces
-  if (pressed_ticks < debounceTimeout) goto defer;
-
-  // Handle LONG PRESS
-  if (pressed_ticks > longPressTimeout) {
-    direction = !direction;
-    goto defer;
+  // Check if the interrupt is for rising edge (Press)
+  if (button_sw1_pressed()) {
+    last_press_tick = ticks;
+  } else { // Interrupt for falling edge (Release)
+    last_release_tick = ticks;
+    process_press();
   }
-
-  // If auto mode is enabled, disable it
-  if (mode == AUTO) {
-    mode = MANUAL;
-    goto defer;
-  }
-
-  // Check for another click within the double click timeout
-  bool doubleClick = false;
-
-  // Reset timer
-  ticks = 0;
-
-  // Check if the button is pressed again after the debounce timeout
-  while (ticks < doubleClickTimeout) {
-    if (button_pressed() && ticks > debounceTimeout) {
-
-      // Wait for button release
-      while (button_pressed());
-
-      if (mode == MANUAL) mode = AUTO;
-      else mode = MANUAL;
-
-      doubleClick = true;
-      break;
-    }
-  }
-
-  // If it is not a double click, then it was a single click
-  if (!doubleClick) {
-    ment(); // Increment or decrement counter on single click
-  }
-
-defer:
   GPIO_PORTF_ICR_R |= 0x10; // Clear interrupt bit
+}
+
+void IntSysTickHandler(void) {
+  // Hardware clears systick int reguest
+  ticks++; // Increment ticks every 1ms
+
+  // Handle auto mode
+  if (mode == AUTO) {
+    if (ticks % AUTO_MODE_INTERVAL == 0) // If 0.2 second has passed
+      ment();                            // Increment or decrement counter
+  }
 }
 
 int main(void) {
   setup();
 
+  // Main loop
   while (1) {
     setLEDColor(colors[counter]);
-    if (mode == AUTO) {
-        if (ticks > autoModeInterval) {
-            ticks -= autoModeInterval;
-            ment();
-        }
-    }
   }
 
   return (0);
--- assignment_1/systick.h
+++ assignment_1/systick.h
@@ -1,26 +1,17 @@
+// Include guard for systick.h
+#ifndef SYSTICK_H
+#define SYSTICK_H
+
 // Implement EVERYTHING for systick
 
-#include <stdint.h>
 #include "tm4c123gh6pm.h"
+#include <stdint.h>
 
 #define SYSTICK_RELOAD_VALUE 16000 // SysTick interrupt every 1ms
-#define SYSTICK_PRIORITY     0x7E  // Priority of the systick interrupt
-
+#define SYSTICK_PRIORITY 0x7E      // Priority of the systick interrupt
 
 // Volatile because it is changed in an ISR
 volatile uint32_t ticks = 0;
-
-
-void IntSysTickHandler(void) {
-  // Hardware clears systick int reguest
-  ticks++; // Increment ticks every 1ms
-
-  /* // Handle auto mode */
-  /* if (mode == AUTO) { */
-  /*   if (ticks % autoModeInterval == 0) // If 0.2 second has passed */
-  /*     ment();                          // Increment or decrement counter */
-  /* } */
-}
 
 void init_systick() {
 
@@ -33,9 +24,9 @@ void init_systick() {
   NVIC_ST_RELOAD_R = SYSTICK_RELOAD_VALUE;
 
   // Set systick priority to 0x10, first clear then set.
-  NVIC_SYS_PRI3_R &= ~(NVIC_SYS_PRI3_TICK_M);
-  NVIC_SYS_PRI3_R |=
-      (NVIC_SYS_PRI3_TICK_M & (SYSTICK_PRIORITY << NVIC_SYS_PRI3_TICK_S));
+  /* NVIC_SYS_PRI3_R &= ~(NVIC_SYS_PRI3_TICK_M); */
+  /* NVIC_SYS_PRI3_R |= */
+  /*     (NVIC_SYS_PRI3_TICK_M & (SYSTICK_PRIORITY << NVIC_SYS_PRI3_TICK_S)); */
 
   // Select systick clock source, Use core clock
   NVIC_ST_CTRL_R |= NVIC_ST_CTRL_CLK_SRC;
@@ -46,3 +37,4 @@ void init_systick() {
   // Enable and start timer
   NVIC_ST_CTRL_R |= NVIC_ST_CTRL_ENABLE;
 }
+#endif
