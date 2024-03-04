598aaa37f91fc240dd115eebdc2af4df8d2fd2d6
BalderHolst <balderwh@gmail.com>
2024-03-04 14:17:07 +0100

    lab2

---
+++ lab2/main.c
@@ -0,0 +1,39 @@
+#include "emp_type.h"
+#include "gpio.h"
+#include "uart.h"
+
+#include "tm4c123gh6pm.h"
+#include "systick.h"
+#include <stdint.h>
+
+#define TIMEOUT 1000
+
+volatile INT16U ticks;
+
+void SysTick_Handler(void) {
+    if (ticks) ticks--;
+}
+
+void setup() {
+    setupPortF();
+    setLEDColor(GREEN);
+    init_systick();
+    uart_init();
+}
+
+int main(void) {
+    setup();
+
+    while(1) {
+        uart_put_stringln("Turning ON!\n");
+        setLEDColor(BLUE);
+        ticks = TIMEOUT;
+        while (ticks);
+        uart_put_stringln("Turning OFF...\n");
+        setLEDColor(OFF);
+        ticks = TIMEOUT;
+        while (ticks);
+    }
+
+    return (0);
+}
---
+++ lab2/module.c
@@ -0,0 +1,25 @@
+/***********************************************
+ * Univeristy of Southern Denmark
+ * Embedded Programming (EMP)
+ *
+ * MODULENAME: module.c
+ * PROJECT: template c-file
+ * DESCRIPTION: Empty module template
+ * Change log:
+ ***********************************************
+ * Date of Change
+ * YYMMDD
+ * ----------------
+ * 160224 MB Module created.
+ ************************************************/
+
+/***************** Header *********************/
+/***************** Include files **************/
+#include "module.h"
+/***************** Defines ********************/
+/***************** Constants ******************/
+/***************** Variables ******************/
+INT8U dummy1;
+/***************** Functions ******************/
+void test1(void) { dummy1++; }
+/***************** End of module **************/
